6772c7bfc8a21bb4604990cbccaa29bea2dd8594
martin <lmapii@gmail.com>
2023-07-07 09:55:36 +0200

    tutorial: dummy subsystem

---
+++ include/dummy/dummy.h
@@ -0,0 +1,19 @@
+/**
+ * \file dummy.h
+ *
+ * \brief A dummy subsystem.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint8_t dummy_random(void);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/dummy.c
@@ -0,0 +1,10 @@
+/**
+ * \file dummy.c
+ */
+
+#include "dummy/dummy.h"
+
+uint8_t dummy_random(void)
+{
+    return 4U; // determined by fair dice roll.
+}
